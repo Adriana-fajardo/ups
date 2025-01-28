376fa37c4c5e61b5ed97ba93a0cbe6978b859814
Adriana_fajardo <afajardoh@est.ups.edu.ec>
2025-01-28 09:39:01 -0500

    Add(I): Add search id method

--- src/main/java/com/example/ups/poo/service/PersonService.java
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -1,14 +1,20 @@
 package com.example.ups.poo.service;
 
 import com.example.ups.poo.dto.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PersonService {
 
+    private static Object getAllPeople;
     List<Person> personList = new ArrayList<>();
 
 
@@ -23,8 +29,17 @@ public class PersonService {
         personList.add(p2);
         return personList;
     }
-}
+    public ResponseEntity getPersonById(String id){
+        for (Person person: personList){
+            if(id.equalsIgnoreCase(person.getId())){
+                return ResponseEntity.status(HttpStatus.OK).body(person);
+            }
+        }
+        String message = "Person with id: " +id + " not found";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
 
+    }
+}
 
 
 
