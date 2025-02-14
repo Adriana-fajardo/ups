61724cd70dbfb6051cdfb9fb4a92a92fe2bb3474
Adriana-Fajardo <afajardoh@est.ups.edu.ec>
2025-02-14 09:56:57 -0500

    Add(I):Add Implement Person Crud

--- src/main/java/com/example/ups/poo/controllers/PersonController.java
+++ src/main/java/com/example/ups/poo/controllers/PersonController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class PersonController {
     private final PersonService personService;
 
-
     public PersonController(PersonService personService) {
-
         this.personService = personService;
     }
 
@@ -19,18 +17,24 @@ public class PersonController {
     public ResponseEntity getAllPeople() {
         return personService.getAllPeople();
     }
+
+    @GetMapping("/get-person")
+    public ResponseEntity getPersonById(@RequestParam String id) {
+        return personService.getPersonById(id);
+    }
+
+    @PostMapping("/person")
+    public ResponseEntity createPerson(@RequestBody PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
+    }
+
+    @PutMapping("/update-person")
+    public ResponseEntity updatePerson(@RequestBody PersonDTO personDTO) {
+        return personService.updatePerson(personDTO);
+    }
+
+    @DeleteMapping("/delete-person")
+    public ResponseEntity deletePerson(@RequestParam String id) {
+        return personService.deletePersonById(id);
+    }
 }
-//
-//    @PostMapping("/person")
-//    public ResponseEntity createPerson(@RequestBody PersonDTO person){
-//        return personService.createPerson(person);
-//    }
-//    @PutMapping("/update-person")
-//    public ResponseEntity updatePerson(@RequestBody PersonDTO Person){
-//        return personService.updatePerson(Person);
-//    }
-//    @GetMapping("/get-person")
-//    public ResponseEntity getPersonById (@RequestParam String id){
-//        return personService.getPersonById(id);
-//    }
-//}
